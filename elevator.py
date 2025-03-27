24fbeee4d0d0f97c4c455ff4b67c47b3ff30d886
Stephen Smithwick <stephen.smithwick@gmail.com>
2025-03-27 16:15:09 -0600

    Removed unnecesary Building class and added Routes to handle mergeing routes details in a single direction

--- elevator.py
+++ elevator.py
@@ -1,40 +1,5 @@
 from enum import Enum
-
-class Building:
-
-    """A Building has a collection of floors and elevators.
-
-    Make a #request for an elevator and #wait for the elevators to move.
-    """
-
-    def __init__(self, top: int, bottom: int, elevators: list):
-        """Initialize Building with elevators servicing all floors.
-
-        :top: The top floor of the building
-        :bottom: The bottom floor of the building
-        :elevators: A list of where all the elevators are
-        """
-        self.elevators = [
-            Elevator(top=top, bottom=bottom, floor=floor)
-            for floor in elevators
-        ]
-
-    def request(self, start: int, end: int) -> None:
-        """Send a request for an elevator to stop at a particular floor.
-
-        :start: The floor the elevator will pick up passengers from
-        :end: The floor the elevator will drop passengers at
-        """
-        self.elevators[0].request(start = start, end = end)
-
-    def wait(self, ticks: int = 1)-> None:
-        """Wait for the elevators to move.
-
-        :ticks: The amount of elevator movements to wait. default = 1
-        """
-        for _ in range(ticks):
-            for elevator in self.elevators:
-                elevator.move()
+from heapq import merge
 
 class Door(Enum):
     """The door state."""
@@ -42,22 +7,76 @@ class Door(Enum):
     OPEN = 1
     SHUT = 2
 
-class Elevator:
-    """A single elevator which will move to target levels and Open/shut the door."""
+class Route:
+    """A series of Elevator Stops in the same direction."""
 
-    def __init__(self, top: int, bottom: int, floor: int, door: Door = Door.SHUT):
+    def __init__(self, start: int, end: int):
+        """Initialize route with initial request details.
+
+        :start: The start of the route
+        :end: The end of the route
+        """
+        self.started = False
+        self.up = start < end
+        self.stops = [start, end]
+
+    def start_if_ready(self, current: int) -> None:
+        """Mark the route as started if th.
+
+        Started routes can only grow up to the current level when merging.
+        """
+        self.started = (
+            self.started
+            or (self.up and current <= self.stops[0])
+            or (not self.up and current >= self.stops[0])
+        )
+
+    def mergable(self, current_level: int, start: int, end: int) -> bool:
+        """Return if route is eligible to be merged with requested start & end."""
+        if self.up and self.started:
+            return current_level <= start < end
+        if self.up and not self.started:
+            return start < end
+        if not self.up and self.started:
+            return current_level >= start > end
+        return start > end
+
+    def merge(self, start: int, end: int) -> None:
+        """Merge the start and end levels into the route and remove duplicate stops."""
+        if self.up:
+            merged = list(merge(self.stops, [start, end]))
+        else:
+            merged = list(merge(self.stops, [start, end], reverse = True))
+        self.stops = [merged[0]] + [
+            stop for prev, stop
+            in zip(merged, merged[1:])
+            if prev != stop
+        ]
+
+    def next_stop(self) -> int:
+        """Return the next stop."""
+        return self.stops[0]
+
+    def empty(self) -> bool:
+        """Return true if there are no more stops in the route."""
+        return not self.stops
+
+    def pop(self) -> int:
+        """Remove the current stop from the route."""
+        return self.stops.pop(0)
+
+class Elevator:
+    """A single elevator which will move to requested levels and open/shut the door."""
+
+    def __init__(self, level: int, door: Door = Door.SHUT):
         """Initialize Elevator with range and state.
 
-        :top: The top floor this elevator goes to
-        :bottom: The bottom floor this elevator goes to
         :floor: The floor this elevator is currently on
         :door: The state of the elevator door
         """
-        self.floor = floor
-        self.top = top
-        self.bottom = bottom
+        self.level = level
         self.door = door
-        self.targets = []
+        self.routes = []
 
     def request(self, start: int, end: int) -> None:
         """Request that this elevator picks up a passenger.
@@ -65,19 +84,32 @@ class Elevator:
         :start: where to pick up the passenger
         :end: where to drop off the passenger
         """
-        self.targets.extend([start, end])
+        if merge_route := next((
+            r for r in self.routes
+            if r.mergable(self.level, start, end)
+        ), None):
+            merge_route.merge(start, end)
+        else:
+            route = Route(start, end)
+            if not self.routes:
+                route.start_if_ready(self.level)
+            self.routes.append(route)
 
     def move(self) -> None:
-        """Allow an elevator to make one move according to it's targets.
+        """Allow an elevator to make one move according to it's stops.
 
         An elevator will either OPEN/CLOSE it's doors or move up/down a level
         """
         if self.door == Door.OPEN:
             self.door = Door.SHUT
-        elif self.targets:
-            target = self.targets[0]
-            if target == self.floor:
+        elif self.routes:
+            route = self.routes[0]
+            if route.next_stop() == self.level:
                 self.door = Door.OPEN
-                self.targets.pop(0)
+                route.pop()
+                if route.empty():
+                    self.routes.pop(0)
+                else:
+                    route.start_if_ready(self.level)
             else:
-                self.floor +=  1 if target > self.floor else -1
+                self.level += 1 if route.next_stop() > self.level else -1
--- elevator_test.py
+++ elevator_test.py
@@ -1,46 +1,169 @@
-from elevator import Door, Elevator
-
+from elevator import Door, Elevator, Route
 
 class TestElevator:
-    def test_elevator_request_adds_a_targetfor_start_and_end(self):
-        elevator = Elevator(floor=0, top=20, bottom=-2)
-        elevator.request(start = -1, end = 0)
-
-        assert elevator.floor == 0
-        assert elevator.door == Door.SHUT
-        assert elevator.targets == [-1, 0]
-
-    def test_elevator_opens_door_on_target(self):
-        elevator = Elevator(floor=0, top=20, bottom=-2, door=Door.SHUT)
+    def test_move_opens_door_on_target(self):
+        elevator = Elevator(level=0, top=20, bottom=-2, door=Door.SHUT)
         elevator.request(start = 0, end = 1)
 
         elevator.move()
 
         assert elevator.door == Door.OPEN
 
-    def test_elevator_closes_open_door(self):
-        elevator = Elevator(floor=0, top=20, bottom=-2, door=Door.OPEN)
+    def test_move_closes_open_door(self):
+        elevator = Elevator(level=0, top=20, bottom=-2, door=Door.OPEN)
 
         elevator.move()
 
         assert elevator.door == Door.SHUT
 
-    def test_elevator_travels_opens_door_at_request_start(self):
-        elevator = Elevator(top=20, bottom=-2, floor=-1)
+    def test_move_opens_door_at_request_start(self):
+        elevator = Elevator(top=20, bottom=-2, level=-1)
 
         elevator.request(start = 0, end = 20)
         for _ in range(2):
             elevator.move()
 
-        assert elevator.floor == 0
+        assert elevator.level == 0
         assert elevator.door == Door.OPEN
 
-    def test_elevator_travels_opens_door_at_request_end(self):
-        elevator = Elevator(top=20, bottom=-2, floor=-1)
+    def test_move_opens_door_at_request_end(self):
+        elevator = Elevator(top=20, bottom=-2, level=-1)
 
         elevator.request(start = 0, end = 20)
         for _ in range(24):
             elevator.move()
 
-        assert elevator.floor == 20
+        assert elevator.level == 20
         assert elevator.door == Door.OPEN
+
+    def test_request_adds_a_target_for_start_and_end(self):
+        elevator = Elevator(level=0, top=20, bottom=-2)
+
+        elevator.request(start = -1, end = 0)
+
+        assert show_plan(elevator) == [[-1, 0]]
+
+    def test_request_interleves_start_if_on_the_way(self):
+        elevator = Elevator(level=1, top=20, bottom=-2)
+        elevator.request(start = -1, end = 3)
+
+        elevator.request(start = 0, end = 5)
+
+        assert show_plan(elevator) == [[-1, 0, 3, 5]]
+
+    def test_request_interleves_end_if_on_the_way(self):
+        elevator = Elevator(level=1, top=20, bottom=-2)
+        elevator.request(start = -1, end = 3)
+        elevator.request(start = 4, end = 8)
+
+        elevator.request(start = 0, end = 5)
+
+        assert show_plan(elevator) == [[-1, 0, 3, 4, 5, 8]]
+
+    def test_request_dedupes_stops_on_the_way(self):
+        elevator = Elevator(level=1, top=20, bottom=-2)
+        elevator.request(start = -1, end = 3)
+
+        elevator.request(start = -1, end = 3)
+
+        assert show_plan(elevator) == [[-1, 3]]
+
+    def test_request_internal_to_path_is_added(self):
+        elevator = Elevator(level=1, top=20, bottom=-2)
+        elevator.request(start = -1, end = 5)
+
+        elevator.request(start = 1, end = 3)
+
+        assert show_plan(elevator) == [[-1, 1, 3, 5]]
+
+    def test_request_continue_from_last_stop(self):
+        elevator = Elevator(level=1, top=20, bottom=-2)
+        elevator.request(start = 0, end = 5)
+
+        elevator.request(start = 5, end = 0)
+
+        assert show_plan(elevator) == [[0, 5], [5, 0]]
+
+    def test_request_which_must_make_elevator_go_higher_before_turning(self):
+        elevator = Elevator(level=1, top=20, bottom=-2)
+        elevator.request(start = -1, end = 5)
+        elevator.request(start = 5, end = 0)
+
+        elevator.request(start = 6, end = 0)
+
+        assert show_plan(elevator) == [[-1, 5], [6, 5, 0]]
+
+    def test_request_needs_more_than_2_routes(self):
+        elevator = Elevator(level=1, top=20, bottom=-2)
+        elevator.request(start = -1, end = 5)
+        elevator.request(start = 5, end = 0)
+
+        elevator.request(start = 6, end = 0)
+        elevator.request(start = 0, end = -3)
+
+        assert show_plan(elevator) == [[-1, 5], [6, 5, 0, -3]]
+
+
+class TestRoute:
+    def test_start_if_ready_down(self):
+        route = Route(2, 1)
+
+        route.start_if_ready(1)
+        assert route.started == False
+
+        route.start_if_ready(2)
+        assert route.started == True
+
+    def test_start_if_ready_up(self):
+        route = Route(1, 2)
+
+        route.start_if_ready(2)
+        assert route.started == False
+
+        route.start_if_ready(1)
+        assert route.started == True
+
+    def test_start_if_ready_is_sticky(self):
+        route = Route(1, 2)
+
+        route.start_if_ready(1)
+        assert route.started == True
+
+        route.start_if_ready(2)
+        assert route.started == True
+
+    def test_mergable_is_true_for_all_requests_in_same_direction(self):
+        route = Route(1, 2)
+        route.started = False
+
+        assert route.mergable(current_level=1, start=0, end=5) == True
+
+    def test_mergable_is_false_for_requests_before_current_level_if_started(self):
+        route = Route(1, 2)
+        route.started = True
+
+        assert route.mergable(current_level=1, start=0, end=5) == False
+
+    def test_merge_combines_request_with_route_going_up(self):
+        route = Route(1, 3)
+
+        route.merge(2, 4)
+
+        assert route.stops == [1, 2, 3, 4]
+
+    def test_merge_combines_request_with_route_going_down(self):
+        route = Route(10, 8)
+
+        route.merge(9, 4)
+
+        assert route.stops == [10, 9, 8, 4]
+
+    def test_merge_removes_duplicate_stops(self):
+        route = Route(10, 9)
+
+        route.merge(9, 8)
+
+        assert route.stops == [10, 9, 8]
+
+def show_plan(elevator):
+    return [r.stops for r in elevator.routes]
